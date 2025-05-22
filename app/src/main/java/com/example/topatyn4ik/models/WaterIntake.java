eb965c3cbe7e2e4aaaca0f60c08894d7eb541688
Jaguar0606official <lehanik3006@gmail.com>
2025-05-22 16:23:03 +0300

    Реализация класса модели WaterIntake.java

--- app/src/main/java/com/example/topatyn4ik/models/WaterIntake.java
+++ app/src/main/java/com/example/topatyn4ik/models/WaterIntake.java
@@ -1,4 +1,21 @@
 package com.example.topatyn4ik.models;
 
+import java.util.Date;
+
 public class WaterIntake {
+    private int amount;
+    private Date timestamp;
+
+    public WaterIntake(int amount, Date timestamp) {
+        this.amount = amount;
+        this.timestamp = timestamp;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+
+    public Date getTimestamp() {
+        return timestamp;
+    }
 }
